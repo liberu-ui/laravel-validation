03170b820a129573b781d649bc6d7c57c4948a1e
mishu <mishu@xtelecom.ro>
2020-07-28 12:09:12 +0300

    adds first() method

--- src/Errors.js
+++ src/Errors.js
@@ -9,18 +9,6 @@ class Errors {
         return Object.keys(this.errors).length > 0;
     }
 
-    has(field) {
-        return Object.keys(this.errors).includes(field);
-    }
-
-    get(field) {
-        return this.has(field) ? this.errors[field][0] : null;
-    }
-
-    set(errors) {
-        this.errors = errors;
-    }
-
     clear(field) {
         if (this.has(field)) {
             Vue.delete(this.errors, field);
@@ -29,6 +17,24 @@ class Errors {
 
     empty() {
         Object.keys(this.errors).forEach(field => this.clear(field));
+    }
+
+    first() {
+        const [field = null] = Object.keys(this.errors);
+
+        return this.get(field);
+    }
+
+    get(field) {
+        return this.has(field) ? this.errors[field][0] : null;
+    }
+
+    has(field) {
+        return Object.keys(this.errors).includes(field);
+    }
+
+    set(errors) {
+        this.errors = errors;
     }
 }
 
